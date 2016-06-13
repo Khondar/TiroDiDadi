52b204c528231a336aad842a96dd792d1bdc7ae6
reushe17 <giacomo.pizzella95@gmail.com>
2016-06-13 16:42:28 +0200

    creata classe che estende SQLiteOpenHelper

---
+++ app/src/main/java/com/example/basil/dicelauncher/StorageOpenHelper.java
@@ -0,0 +1,21 @@
+package com.example.basil.dicelauncher;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by giacomo on 13/06/2016.
+ */
+public class StorageOpenHelper extends SQLiteOpenHelper {
+
+    public StorageOpenHelper (Context context) {
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+}
