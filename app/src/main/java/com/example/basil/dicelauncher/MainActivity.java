aca6e06369ad76dc438e3bd54c7c5db156df13ea
Khondar <angelo.basile@gmx.com>
2016-06-16 16:01:50 +0200

    Database a mano

--- app/src/main/java/com/example/basil/dicelauncher/MainActivity.java
+++ app/src/main/java/com/example/basil/dicelauncher/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.basil.dicelauncher;
 
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     PlayerNameFragment nomeFragment;
     SelectDicePlayerFragment sceltaPlayer;
     BackFragment backFragment;
+    SharedPreferences sharP;
+    final static private String SHARP = "com.MainActivity.Sharp";
+    final static private String PREFERENCE = "com.MainActivity.Preference";
+    int variabile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +28,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if(savedInstanceState != null){
-            //Ripristina l'istanza del fragment
-            dadi = (SelezioneDatiFragment)getSupportFragmentManager().getFragment(savedInstanceState, "mContent1");
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentDadi, dadi, "dadi").addToBackStack(null).commit();
+            sharP = getBaseContext().getSharedPreferences(PREFERENCE, 0);
+            variabile = sharP.getInt(SHARP, 0);
+            switch (variabile){
+                case 1:
+                    dadi = (SelezioneDatiFragment)getSupportFragmentManager().getFragment(savedInstanceState, "mContent1");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentDadi, dadi, "dadi").addToBackStack(null).commit();
 
-            menu = (MenuFragment)getSupportFragmentManager().getFragment(savedInstanceState, "mContent2");
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentMenu, menu, "menu").addToBackStack(null).commit();
-        }else{
+                    menu = (MenuFragment)getSupportFragmentManager().getFragment(savedInstanceState, "mContent2");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentMenu, menu, "menu").addToBackStack(null).commit();
+                    break;
+                case 2:
+                    dadi = (SelezioneDatiFragment)getSupportFragmentManager().getFragment(savedInstanceState, "mContent1");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentDadi, dadi, "dadi").addToBackStack(null).commit();
+
+                    nomeFragment = (PlayerNameFragment)getSupportFragmentManager().getFragment(savedInstanceState, "mContent3");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentMenu, nomeFragment, "nomeEditor").addToBackStack(null).commit();
+                    break;
+                case 3:
+                    sceltaPlayer = (SelectDicePlayerFragment) getSupportFragmentManager().getFragment(savedInstanceState, "mContent4");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentDadi, sceltaPlayer, "sceltaPlayer").addToBackStack(null).commit();
+
+                    backFragment = (BackFragment) getSupportFragmentManager().getFragment(savedInstanceState, "mContent5");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentMenu, backFragment, "backFragment").addToBackStack(null).commit();
+                    break;
+                default:
+                    dadi = new SelezioneDatiFragment();
+                    getSupportFragmentManager().beginTransaction().add(R.id.fragmentDadi, dadi, "dadi").addToBackStack(null).commit();
+                    menu = new MenuFragment();
+                    getSupportFragmentManager().beginTransaction().add(R.id.fragmentMenu, menu, "menu").addToBackStack(null).commit();
+                    break;
+            }
+        } else {
             dadi = new SelezioneDatiFragment();
             getSupportFragmentManager().beginTransaction().add(R.id.fragmentDadi, dadi, "dadi").addToBackStack(null).commit();
             menu = new MenuFragment();
             getSupportFragmentManager().beginTransaction().add(R.id.fragmentMenu, menu, "menu").addToBackStack(null).commit();
-            nomeFragment = new PlayerNameFragment();
-            sceltaPlayer = new SelectDicePlayerFragment();
-            backFragment = new BackFragment();
         }
-
+        nomeFragment = new PlayerNameFragment();
+        sceltaPlayer = new SelectDicePlayerFragment();
+        backFragment = new BackFragment();
         getSupportActionBar().hide();
 
     }
@@ -48,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
         filter.addAction(DiceAndRollBroadcast.Action.ACTION_ROLL_DICE);
 
         LocalBroadcastManager.getInstance(this).registerReceiver(broadcast, filter);
-
+        Log.d("Main Activity", "onStart");
         startService((new Intent(this, ShakeAndRollService.class)).setAction(ShakeAndRollService.NULLA));
         super.onStart();
     }
@@ -58,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcast);
         stopService(new Intent(getBaseContext(), ShakeAndRollService.class));
+        Log.d("Main Activity", "onStop");
         super.onStop();
     }
 
@@ -67,6 +98,10 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().putFragment(outState,"mContent1", dadi);
         getSupportFragmentManager().putFragment(outState,"mContent2", menu);
+        getSupportFragmentManager().putFragment(outState, "mContent3", nomeFragment);
+        getSupportFragmentManager().putFragment(outState, "mContent4", sceltaPlayer);
+        getSupportFragmentManager().putFragment(outState, "mContent5", backFragment);
+
     }
 
     private DiceAndRollBroadcast broadcast = new DiceAndRollBroadcast() {
@@ -75,24 +110,28 @@ public class MainActivity extends AppCompatActivity {
             Intent serviceIntent = new Intent(MainActivity.this, ShakeAndRollService.class);
             String action = "no";
             String id = message;
+            sharP = getBaseContext().getSharedPreferences(PREFERENCE, 0);
+            SharedPreferences.Editor edit = sharP.edit();
             switch (tag) {
                 case MenuFragment.ROLL:
                     serviceIntent.setAction(ShakeAndRollService.ROLL);
                     comandaIlService(serviceIntent);
                     comandaIDadi().rollDice();
+                    edit.putInt(SHARP, 1).commit();
                     break;
                 case MenuFragment.SAVE:
-
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragmentMenu, nomeFragment, "nomeEditor").addToBackStack(null).commit();
                     serviceIntent.setAction(ShakeAndRollService.SAVE);
                     comandaIlService(serviceIntent);
                     comandaIDadi().saveDice();
+                    edit.putInt(SHARP, 2).commit();
                     break;
                 case MenuFragment.LOAD:
                     serviceIntent.setAction(ShakeAndRollService.LOAD);
                     comandaIlService(serviceIntent);
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragmentDadi, sceltaPlayer, "sceltaPlayer").addToBackStack(null).commit();
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragmentMenu, backFragment, "backFragment").addToBackStack(null).commit();
+                    edit.putInt(SHARP, 3).commit();
                     break;
 
 
@@ -109,11 +148,13 @@ public class MainActivity extends AppCompatActivity {
                     comandaIlService(serviceIntent);
                     break;
 
+
                 case PlayerNameFragment.NAME:
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragmentMenu, menu, "menu").addToBackStack(null).commit();
                     comandaIDadi().insertName(id);
                     serviceIntent.setAction(ShakeAndRollService.SAVE);
                     comandaIlService(serviceIntent);
+                    edit.putInt(SHARP, 1).commit();
                     break;
 
                 case SacchettaAdatper.LOAD:
@@ -122,6 +163,7 @@ public class MainActivity extends AppCompatActivity {
                     serviceIntent.setAction(ShakeAndRollService.LOAD);
                     comandaIlService(serviceIntent);
                     comandaIDadi().loadDice(id);
+                    edit.putInt(SHARP, 1).commit();
                     break;
                 case SacchettaAdatper.DELETE:
                     SelectDicePlayerFragment selezionaPersonaggi = (SelectDicePlayerFragment) getSupportFragmentManager().findFragmentByTag("sceltaPlayer");
@@ -131,6 +173,7 @@ public class MainActivity extends AppCompatActivity {
                 case BackFragment.RETURN_BACK:
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragmentDadi, dadi, "dadi").addToBackStack(null).commit();
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragmentMenu, menu, "menu").addToBackStack(null).commit();
+                    edit.putInt(SHARP, 1).commit();
                 default:
                     break;
             }
