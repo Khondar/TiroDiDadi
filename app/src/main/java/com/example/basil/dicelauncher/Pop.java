83766981011da31b261bec0535cb75fd76a4366f
Danilo <danilozuccala@libero.it>
2016-06-17 12:59:10 +0200

    ultime modifiche commentate

---
+++ app/src/main/java/com/example/basil/dicelauncher/Pop.java
@@ -0,0 +1,26 @@
+package com.example.basil.dicelauncher;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by Danilo on 17/06/2016.
+ */
+public class Pop extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.popwindow);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int)(width*.8),(int)(height*.8));
+
+    }
+
+}
